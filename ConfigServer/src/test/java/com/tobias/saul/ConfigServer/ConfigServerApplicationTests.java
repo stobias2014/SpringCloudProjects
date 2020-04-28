57548527685434deab6c9ef75bf3caedc9354939
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-28 14:56:28 -0500

    added local config server

---
+++ ConfigServer/src/test/java/com/tobias/saul/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
