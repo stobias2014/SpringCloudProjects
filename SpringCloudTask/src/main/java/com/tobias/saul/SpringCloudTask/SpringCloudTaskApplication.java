37a846428146b66821374e7c660a551424425723
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-04 16:10:12 -0500

    added spring cloud task proj

---
+++ SpringCloudTask/src/main/java/com/tobias/saul/SpringCloudTask/SpringCloudTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.SpringCloudTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTaskApplication.class, args);
+	}
+
+}
