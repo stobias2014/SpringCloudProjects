95f8d588f8a9aecf9789de8ee44dd8ab56bb618b
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-04 17:18:45 -0500

    added TaskSink Project

---
+++ TaskSink/src/main/java/com/tobias/saul/TaskSink/TaskSinkApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.TaskSink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSinkApplication.class, args);
+	}
+
+}
---
+++ TaskSink/src/test/java/com/tobias/saul/TaskSink/TaskSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.TaskSink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
