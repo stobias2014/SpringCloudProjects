9c6dd5bd64bf2850417bb0956c978bbbdc357618
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-05 15:01:59 -0500

    changed rabbit mq prop value to 5672

--- TaskSink/src/main/java/com/tobias/saul/TaskSink/TaskSinkApplication.java
+++ TaskSink/src/main/java/com/tobias/saul/TaskSink/TaskSinkApplication.java
@@ -2,10 +2,13 @@ package com.tobias.saul.TaskSink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.task.launcher.annotation.EnableTaskLauncher;
 
 @SpringBootApplication
 @EnableTaskLauncher
+//@EnableBinding(Sink.class)
 public class TaskSinkApplication {
 
 	public static void main(String[] args) {
